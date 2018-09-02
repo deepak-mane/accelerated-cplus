b43a265633488a00cd6d4c11e81c1b089c4aafb3
osboxes.org <deepak.mane123@gmail.com>
2018-09-02 05:43:27 -0400

    Initial Update

---
+++ chap1_strings/1.1_input.cpp
@@ -0,0 +1,30 @@
+/*
+ * 1.1_input.cpp
+ *
+ *  Created on: Sep 2, 2018
+ *      Author: osboxes
+ */
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cout << "Please enter your first name: ";
+
+	std::string name;
+	std:: cin >> name;
+
+    const std::string greeting = " Hello, " + name + "! ";
+    const std::string spaces(greeting.size(), ' ' );
+    const std::string second = "* " + spaces + " *";
+    const std::string first(second.size(), '*');
+
+    std::cout << std::endl;
+    std::cout  << first << std::endl;
+    std::cout << second << std::endl;
+    std::cout << "* " << greeting << " *" << std::endl;
+    std::cout << second << std::endl;
+    std::cout  << first << std::endl;
+
+	return 0;
+}
